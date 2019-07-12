da4927b3b76aa0e198a1a8cf34194743eb74604b
max <mpano91@gmail.com>
2019-07-12 19:32:54 +0300

    src/list.c: add function

--- src/list.c
+++ src/list.c
@@ -34,16 +34,31 @@ struct node {
 
 ccl_linked_list_t *cclListInit(const size_t data_size)
 {
+    ccl_list_t *init;
+
+    if (!data_size) {
+        return NULL;
+    }
+
+    init = ccl_malloc(sizeof(ccl_list_t));
+    init->head = NULL;
+    init->tail = NULL;
+    init->node_size = data_size;
+    init->count = 0;
+
+    return init;
 }
 
 
 int cclListCount(const ccl_list_t *dllist)
 {
+    return dllist->count;
 }
 
 
 int cclListEmpty(const ccl_list_t *dllist)
 {
+    return cclListCount(dllist) == 0;
 }
 
 
@@ -61,19 +76,89 @@ void *cclListIterData(iter_list_t *it)
 {
 }
 
+static struct node *cclListGetNodeFromHead(ccl_list_t *bllist, const int idx)
+{
+    struct node *temp = bllist->head;
+    int i = 0;
+    for (i = 0; i < idx; ++i) {
+        temp = temp->next;
+    }
+
+    return temp;
+}
+
+static struct node *cclListGetNodeFromTail(ccl_list_t *bllist, const int idx)
+{
+    struct node *temp = bllist->tail;
+    int i = 0;
+    for (i = bllist->count - 1; i > idx; --i) {
+        temp = temp->prev;
+    }
+
+    return temp;
+
+}
+
+static struct node *cclListGetNodeAt(ccl_list_t *bllist, const int idx)
+{
+    if (idx < (cclListCount(bllist) / 2)) {
+        return cclListGetNodeFromHead(bllist, idx);
+    }
+
+    return cclListGetNodeFromTail(bllist, idx);
+}
 
 int cclListAddFirst(ccl_list_t *bllist, void *const data)
 {
+    return cclListAddAt(bllist, data, 0);
 }
 
 
 int cclListAddLast(ccl_list_t *bllist, void *const data)
 {
+    return cclListAddAt(bllist, data, bllist->count);
 }
 
 
 int cclListAddAt(ccl_list_t *bllist, void *const data, const int idx)
 {
+    if (idx < 0 || idx > bllist->count) {
+        return -1; //bad args
+    }
+
+    struct node *item = ccl_malloc(sizeof(struct node));
+    item->data = ccl_malloc(bllist->node_size);
+
+    memcpy(item->data, data, bllist->node_size);
+
+    if (!bllist->head) {
+        item->next = NULL;
+        item->prev = NULL;
+        bllist->head = item;
+        bllist->tail = item;
+    } else if (idx == 0) {
+        struct node *const temp = bllist->head;
+        temp->prev = item;
+        item->next = temp;
+        item->prev = NULL;
+        bllist->head = item;
+    } else if (idx == bllist->count) {
+        struct node *const temp = bllist->tail;
+        temp-next = item;
+        item->prev = temp;
+        item->next = NULL;
+        bllist->tail = item;
+    } else {
+        struct node *const temp = cclListGetNodeAt(bllist, idx);
+        item->prev = temp->prev;
+        item->next = temp;
+        temp->prev->next = item;
+        temp->prev = item;
+    }
+
+    ++(bllist->count);
+
+    return 0;//all good
 }
 
 
