f556716bc9ccec96949fee17542d28fe31fd77e4
max <mpano91@gmail.com>
2019-11-10 22:56:50 +0300

    Fix ci yml

--- tools/build-ubuntu.sh
+++ tools/build-ubuntu.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y autoconf intltool libtool libglib2.0-dev doxygen make gcc g++ checkinstall
-sudo apt-get install -y libcunit1-dev libcpputest-dev
+apt-get update
+apt-get upgrade -y
+apt-get install -y autoconf intltool libtool libglib2.0-dev doxygen make gcc g++ checkinstall libcunit1-dev libcpputest-dev
 
