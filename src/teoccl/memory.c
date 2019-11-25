684c44f4cb49085ea4dbb01d26bad55acca653ed
Anton Valisiev <si1ver1502@gmail.com>
2019-11-25 20:01:43 +0300

    Cleanup memory utilities.

--- src/teoccl/memory.c
+++ src/teoccl/memory.c
@@ -4,72 +4,75 @@
  *
  * Created on June 27, 2018, 4:18 PM
  */
-#define _POSIX_C_SOURCE 200809L // strdup warning
 
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 
-static
-void ccl_memory_out(void)
-{
+#if defined(__GNUC__)
+#include <stdnoreturn.h>
+#define TEOCCL_NORETURN noreturn
+#elif defined(_MSC_VER)
+// C11 _Noreturn is not supported in MSVC yet.
+#define TEOCCL_NORETURN __declspec(noreturn)
+#else
+#define TEOCCL_NORETURN
+#endif
+
+TEOCCL_NORETURN static void exit_out_of_memory(void) {
     (void)fprintf(stderr, "CCL. Out of memory\n");
     exit(1);
 }
 
-
-void *ccl_malloc(size_t size)
-{
-    void *memory = NULL;
-    if((memory = malloc(size)) == NULL) {
-        ccl_memory_out();
+void *ccl_malloc(size_t size) {
+    void *memory = malloc(size);
+    if (memory == NULL) {
+        exit_out_of_memory();
     }
     
     return memory;
 }
 
-
-void *ccl_calloc(size_t size)
-{
+void *ccl_calloc(size_t size) {
     void *memory = ccl_malloc(size);
     memset(memory, 0, size);
 
     return memory;
 }
 
+char *ccl_strdup(const char *str) {
+#if defined(_MSC_VER)
+    char *newstr = _strdup(str);
+#else
+    char *newstr = strdup(str);
+#endif
 
-char *ccl_strdup(const char *str)
-{
-    char *newstr;
-    if ((newstr = strdup(str)) == NULL) {
-        ccl_memory_out();
+    if (newstr == NULL) {
+        exit_out_of_memory();
     }
 
     return newstr;
 }
 
+char *ccl_strndup(const char *str, size_t len) {
+    char *newstr = ccl_malloc(len + 1);
 
-char *ccl_strndup(char *str, size_t len)
-{
-    char *newstr;
-    if ((newstr = malloc(len + 1)) == NULL) {
-        ccl_memory_out();
-    }
-
+#if defined(_MSC_VER)
+    (void)strncpy_s(newstr, len + 1, str, len);
+#else
     (void)strncpy(newstr, str, len);
+#endif
+
     newstr[len] = '\0';
 
     return (newstr);
 }
 
-
-void *ccl_realloc(void *ptr, size_t size)
-{
-    void *memory;
-    if ((memory = realloc(ptr, size)) == NULL) {
-        ccl_memory_out();
+void *ccl_realloc(void *ptr, size_t size) {
+    void *memory = realloc(ptr, size);
+    if (memory == NULL) {
+        exit_out_of_memory();
     }
 
     return memory;
