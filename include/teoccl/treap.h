9271439857c2c11149984cc792d27347a15d1a80
max <mpano91@gmail.com>
2020-03-09 14:08:43 +0300

    treap init(decart tree)

---
+++ include/teoccl/treap.h
@@ -0,0 +1,24 @@
+/**
+ * \file treap.h
+ * \brief 
+ * \author max <mpano91@gmail.com>
+ *
+ * treap
+ *
+ * Created on Tue Mar 9 19:55:30 2020
+ */
+
+#ifndef TREAP_H
+#define TREAP_H
+
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/teoccl/treap.c
@@ -0,0 +1,6 @@
+#include <string.h>
+#include <errno.h>
+
+#include "teoccl/memory.h"
+
+
