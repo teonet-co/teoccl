88fb7959a76e7ddb71fde2e3f1ed68dd82a8c77a
Anton Vasiliev <si1ver1502@gmail.com>
2020-04-19 15:11:00 +0300

    Fix SIZE_T_MAX is always equals to UINT_MAX.

--- src/teoccl/list.c
+++ src/teoccl/list.c
@@ -6,15 +6,7 @@
 #include "teoccl/list.h"
 
 #ifndef SIZE_T_MAX
-  #if SIZEOF_SIZE_T == SIZEOF_INT
-    #define SIZE_T_MAX UINT_MAX
-  #elif SIZEOF_SIZE_T == SIZEOF_LONG
-    #define SIZE_T_MAX ULONG_MAX
-  #elif SIZEOF_SIZE_T == SIZEOF_LONG_LONG
-    #define SIZE_T_MAX ULLONG_MAX
-  #else
-    #error Unable to determine size of size_t
-  #endif
+  #define SIZE_T_MAX ((size_t)-1)
 #endif
 
 struct ccl_list {
